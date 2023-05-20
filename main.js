19cf10991394af13109100c684ac2e2b83372829
buggythevampireslayer <132746935+buggythevampireslayer@users.noreply.github.com>
2023-05-20 15:34:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
-const codeToExecute = urlParams.get('run');
+const codeToExecute = decodeURIComponent(urlParams.get('run'));
 console.log(urlParams);
-var result = " ";
-try {
-  result = eval(codeToExecute);
-  result = toString(result);
-  if (result == "[object Undefined]") {
-    result == "Error: Object Undefined";
-  }
-} catch (error) {
-  result = "Error: " + toString(error);
-}
 
-document.getElementById("return_span").innerHTML = result;
+const result = eval(codeToExecute)
+
+document.getElementById("return_span").innerHTML = toString(result);
