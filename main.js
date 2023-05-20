30a5672f6591114b8ce03ba5ca83a51de84e8991
buggythevampireslayer <132746935+buggythevampireslayer@users.noreply.github.com>
2023-05-20 16:05:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const codeToExecute = decodeURIComponent(urlParams.get('run')).replace(";","\n")
 try {
   functionToRun = new Function(codeToExecute)
   const result = functionToRun();
-  document.getElementById("return_span").innerHTML += toString(result);
+  document.getElementById("return_span").innerHTML = result;
 }
 catch (error) {
   const result = toString(error);
