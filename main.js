1075a0683b21d1b5b5358d9fdc7d5d73fbf37dc7
buggythevampireslayer <132746935+buggythevampireslayer@users.noreply.github.com>
2023-05-20 15:53:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const urlParams = new URLSearchParams(window.location.search);
-const codeToExecute = urlParams.get('run');
+const codeToExecute = decodeURIComponents(urlParams.get('run'));
 
 var linesOfCode = codeToExecute.split(";");
-linesOfCode.forEach((line, i) => {
-  linesOfCode[i] = decodeURIComponent(line)
-})
+
 
 try {
-  linesOfCode.forEach((line) => {
-    const result = eval(line);
-    document.getElementById("return_span").innerHTML += toString(result);
-  });
-} catch (error) {
+  a = new Function(linesOfCode)
+  const result = a();
+  document.getElementById("return_span").innerHTML += toString(result);
+}
+catch (error) {
   const result = toString(error);
   console.log("Caught Error")
   console.log(error)
